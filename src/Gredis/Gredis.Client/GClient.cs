0d09475bd67098227d34f8fe26082cdd7a178684
Tomas Bosak <bosak.tomas@gmail.com>
2013-05-02 22:12:01 +0200

    Initial project dump.

---
+++ src/Gredis/Gredis.Client/GClient.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack.Redis;
+
+namespace Gredis.Client
+{
+    public static class G
+    {
+        internal static RedisClient Redis;
+
+        public static void Initialize(string host, int port, string password, int database)
+        {
+            Redis = new RedisClient(host, port, password);
+            Redis.Db = database;
+        }
+    }
+}
---
+++ src/Gredis/Gredis.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Gredis.Client;
+
+namespace Gredis.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] connectionString = File.ReadAllText(@"..\..\..\..\..\ConnectionString.txt").Split(';');
+
+            G.Initialize(
+                connectionString[0],
+                int.Parse(connectionString[1]),
+                connectionString[2],
+                int.Parse(connectionString[3])
+            );
+        }
+    }
+}
